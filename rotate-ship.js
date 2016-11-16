3e12750e7339a98a887c4bb922793afcbbe44cc3
Marty Markenson <martyamark@gmail.com>
2016-11-16 14:02:39 -0500

    Add files via upload
    now there is a flag on rotate-ship so it will rotate back an forth.
    issues- ship is placed +1 in z direction when rotated, on edges ship will be off grid when rotated

--- rotate-ship.js
+++ rotate-ship.js
@@ -1,16 +1,28 @@
 AFRAME.registerComponent('rotate-ship', {
     schema: {
     target: {type: 'selector'},
+    flag: {type: 'boolean'},
     },
  
     init: function () {
         var data = this.data;
+        var flag = data.flag;
         var rotation;
         this.el.addEventListener('click', function () {
-            data.target.setAttribute("rotation", {
+            console.log(flag);
+            if(flag) {
+                data.target.setAttribute("rotation", {
                     x:0,
                     y:-90,
                     z:0});
+                flag = false;
+                } else {
+                data.target.setAttribute("rotation", {
+                    x:0,
+                    y:0,
+                    z:0});
+                flag = true;
+                }
             });
-    }
+        }
 });
